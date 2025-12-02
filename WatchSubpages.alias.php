2fcaba1a1ec14193ea56e186d4d757b1c48d32bd
anterdc99 <xuminruixuan@live.cn>
2025-12-03 00:54:58 +0800

    WatchSubpages: Set Chinese primary special page alias to the English primary
    Bug: T399285
    Change-Id: I81129ecbb4e94e7cc013085912f5848e240029e5

--- WatchSubpages.alias.php
+++ WatchSubpages.alias.php
@@ -243,12 +243,17 @@ $specialPageAliases['vi'] = [
 	'WatchSubpages' => [ 'Theo_dõi_trang_phụ' ],
 ];
 
-/** Simplified Chinese (中文（简体）‎) */
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'WatchSubpages' => [ 'WatchSubpages' ],
+];
+
+/** Simplified Chinese (中文（简体）) */
 $specialPageAliases['zh-hans'] = [
 	'WatchSubpages' => [ '监视子页面' ],
 ];
 
-/** Traditional Chinese (中文（繁體）‎) */
+/** Traditional Chinese (中文（繁體）) */
 $specialPageAliases['zh-hant'] = [
 	'WatchSubpages' => [ '監視子頁面' ],
 ];
